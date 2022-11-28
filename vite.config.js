5858e129d0e3b84b07c7eb11e8e2b871dee461ad
otay <otay@solutionware.jp>
2022-11-28 22:08:10 +0900

    baseURL→baseに修正

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  app: {
-    baseURL: '/app-development-training'
-  }
+  base: '/app-development-training'
 })
