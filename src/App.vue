dd20edb2a772f3b5452b7910ab391ace9a66ef8f
otay <otay@solutionware.jp>
2022-12-04 23:46:34 +0900

    おみくじアプリ 全件ループを使用しない実装に修正

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,17 @@
 <script setup>
 import { ref } from 'vue'
-const items = [
-  {
-    id: 1,
-    name: '大吉',
-    description:
-      '大吉です'
-  },
-  {
-    id: 2,
-    name: '中吉',
-    description:
-      '中吉です'
-  },
-  {
-    id: 3,
-    name: '小吉',
-    description:
-      '小吉です'
-  },
-  {
-    id: 4,
-    name: '吉',
-    description:
-      '吉です'
-  },
-  {
-    id: 5,
-    name: '凶',
-    description:
-      '凶です'
-  }
-  ]
-// リアクティブな状態
-const result = ref(0)
+const items = ['大吉','中吉','小吉','吉','凶']
+const resultNum = ref(0)
+const resultVal = ref('')
 
-// 状態を変更し、更新をトリガーする関数。
 function fortune() {
-  const min = 1 ;
-  result.value = Math.floor( Math.random() * (items.length) + min);
+  const min = 0 ;
+  resultNum.value = Math.floor( Math.random() * (items.length) + min);
+  resultVal.value = items[resultNum.value];
 }
-
 </script>
 
 <template>
-  <template v-for="item in items">
-    <div v-if="item.id === result">
-      <h1>{{ item.name }}</h1>
-    </div>
-  </template>
-
+  <h1>{{ resultVal }}</h1>
   <button @click="fortune">おみくじ</button>
 </template>
