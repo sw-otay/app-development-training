75c921eac0bfe8de40b1c1bf189a93ca9af0676a
otay <otay@solutionware.jp>
2023-01-08 18:44:07 +0900

    算出プロパティ使用するよう修正

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 const items = ['大吉','中吉','小吉','吉','凶']
 const resultVal = ref('')
+const resultNum = ref(0)
 
 function makeFortune() {
-  resultVal.value = items[Math.floor( Math.random() * (items.length))];
+  resultNum.value = Math.floor( Math.random() * (items.length));
+  resultVal.value = randomize.value;
 }
+const randomize = computed(() => {
+  return items[resultNum.value]
+})
 </script>
 
 <template>
