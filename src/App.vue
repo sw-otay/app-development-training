7db302adc27785044b3ae7ce898a02a5cba996d7
otay <otay@solutionware.jp>
2023-01-21 18:38:54 +0900

    computedを使用し不要になった行を削除。

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
 import { ref, computed } from 'vue'
 const items = ['大吉','中吉','小吉','吉','凶']
-const resultVal = ref('')
 const resultNum = ref(0)
 
 function makeFortune() {
   resultNum.value = Math.floor( Math.random() * (items.length));
-  resultVal.value = randomize.value;
 }
-const randomize = computed(() => {
+const selectedFortune = computed(() => {
   return items[resultNum.value]
 })
 </script>
 
 <template>
-  <p>{{ resultVal }}</p>
+  <p>{{ selectedFortune }}</p>
   <button @click="makeFortune">おみくじ</button>
 </template>
